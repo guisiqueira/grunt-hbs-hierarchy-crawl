72da94007fd4350be8f0fa3bcf0dc424d4af3ff5
luiz-siqueira <luizgsiqueira@gmail.com>
2015-06-12 14:50:30 -0300

    Reviewed all file and naming convention for grunt plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,7 +32,7 @@ module.exports = function (grunt) {
     },
 
     // Configuration to be run (and then tested).
-    hbs_hierarchy_crawl: {
+    hbshierarchycrawl: {
       default_options: {
         options: {
         },
@@ -55,7 +55,7 @@ module.exports = function (grunt) {
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
   //grunt.registerTask('test', ['clean', 'hbs_hierarchy_crawl', 'nodeunit']);
-  grunt.registerTask('test', ['clean', 'hbs_hierarchy_crawl']);
+  grunt.registerTask('test', ['clean', 'hbshierarchycrawl']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
--- test/hbs-hierarchy-crawl-test.js
+++ test/hbs-hierarchy-crawl-test.js
@@ -22,7 +22,7 @@ var grunt = require('grunt');
     test.ifError(value)
 */
 
-exports.hbs_hierarchy_crawl = {
+exports.hbshierarchycrawl = {
   setUp: function (done) {
     // setup here if necessary
     done();
